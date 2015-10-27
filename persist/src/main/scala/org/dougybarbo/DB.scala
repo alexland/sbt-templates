8ba1cf425bc647c9f28dbff53891ff88ffd53b5b
alexland <doug.ybarbo@gmail.com>
2015-10-27 15:53:06 -0700

    persist code, complete

---
+++ persist/src/main/scala/org/dougybarbo/DB.scala
@@ -0,0 +1,24 @@
+
+package org.dougybarbo
+
+object db {
+
+	println("module db compiled and loaded")
+
+	trait Record {
+		def c1: Int
+		def c2: Int
+	}
+
+	final case class RawDataLine(
+		c1:Int,
+		c2:Int
+	) extends Record
+
+	final case class DbRow(
+		c1: Int,
+		c2: Int,
+		c3: Int
+	) extends Record
+
+}
