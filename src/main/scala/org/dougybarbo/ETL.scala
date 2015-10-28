2e5f71c1805f3caa9a53b7d8b78b067ba2a6045e
alexland <doug.ybarbo@gmail.com>
2015-10-28 15:27:39 -0700

    renamed previously delted src file

---
+++ src/main/scala/org/dougybarbo/ETL.scala
@@ -0,0 +1,17 @@
+
+package org.dougybarbo.submoduletemplate
+
+import org.dougybarbo.streamgraph._
+import org.dougybarbo.persist._
+
+
+
+
+object ETL extends App {
+
+	import org.dougybarbo.streamgraph.StreamGraph
+
+	println("ETL compiled & executed!")
+
+
+}
